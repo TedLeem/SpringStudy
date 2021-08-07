e6723381264ff221af5aad5a52d1d717f35125b9
TedLeem <wjdxor6346@naver.com>
2021-08-07 11:27:44 +0900

    feat: implment userService

--- lab1/User-Service/src/main/java/com/example/usersservice/service/UserServiceImpl.java
+++ lab1/User-Service/src/main/java/com/example/usersservice/service/UserServiceImpl.java
@@ -2,15 +2,33 @@ package com.example.usersservice.service;
 
 
 import com.example.usersservice.dto.UserDto;
+import com.example.usersservice.hibernate.UserEntity;
+import com.example.usersservice.hibernate.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_password");
+
+
+        userRepository.save(userEntity);
 
         return null;
     }
