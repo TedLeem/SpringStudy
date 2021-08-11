e6ef381448a9eeafa56beba5b1d239d179ba8d20
TedLeem <wjdxor6346@naver.com>
2021-08-11 13:40:16 +0900

    feat: update order-service

---
+++ lab1/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,61 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.service.OrderService;
+import com.example.orderservice.vo.RequestOrder;
+import com.example.orderservice.vo.ResponseOrder;
+import org.bouncycastle.math.raw.Mod;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order-service")
+public class OrderController {
+
+    Environment env;
+    OrderService orderService;
+
+    @Autowired
+    public OrderController(Environment env, OrderService orderService) {
+        this.env = env;
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/{userId}/orders")
+    public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId, @RequestBody RequestOrder requestOrder){
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        OrderDto orderDto = mapper.map(requestOrder, OrderDto.class);
+        orderDto.setUserId(userId);
+        orderService.createOrder(orderDto);
+
+        ResponseOrder responseOrder = mapper.map(orderDto,ResponseOrder.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
+    }
+
+    @GetMapping("/{userId}/orders")
+    public ResponseEntity<List<ResponseOrder>> getOrders(@PathVariable("userId") String userId){
+
+        Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
+
+        List<ResponseOrder> resultList = new ArrayList<>();
+
+        orderList.forEach(v ->{
+            resultList.add(new ModelMapper().map(v,ResponseOrder.class));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(resultList);
+
+    }
+}
